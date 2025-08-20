38a31764e0043aa5ba955a25388e4a222f916b03
Nicolas Ludueña <nicolascluduena@gmail.com>
2025-08-19 23:43:27 -0300

    comentarios para mejorar

--- src/core/campaigns/application/editCampaign.ts
+++ src/core/campaigns/application/editCampaign.ts
@@ -1,3 +1,6 @@
+import listByCriteria from "$shared/core/application/listByCriteria";
+import { Criteria } from "$shared/core/domain/criteria/Criteria";
+import { Filter } from "$shared/core/domain/criteria/Filter";
 import { Campaign } from "../domain/campaign";
 import type { ContainerCampaignRepository } from "../domain/ports/ICampaignRepository";
 
@@ -14,8 +17,20 @@ export type EditCampaignInput = {
 
 export async function editCampaign(
     input: EditCampaignInput,
+    //creatorId: number,
     repositories: ContainerCampaignRepository
 ): AsyncResult<Campaign> {
+    // TODO: Aca no se deberia validar que quien trata de editar la campaña sea el creador? también la campaña se puede editar en ciertos estados
+    // Dejo el codigo comentado de como se haría con criteria para evitar hacer los findBy...
+    //const criteria: Criteria = new Criteria();
+    //const filterbyId: Filter = new Filter('id', input.campaignId);
+    // aca podes agregar el filtro de creatorId que se pasa por parametro y en el handler lo recibis por la session
+    //const filterByCreatorId: Filter = new Filter('creator_id', creatorId);
+    // agregas los filtros al criteria
+    //criteria.addFilter(filterbyId);
+    //criteria.addFilter(filterByCreatorId);
+    // usas el caso de uso del shared para buscar la campaña
+    //const campaignResult = await listByCriteria(repositories.campaignRepository, criteria);
     const campaignResult = await repositories.campaignRepository.findById(input.campaignId);
     if (campaignResult.IsErr || !campaignResult.Unwrap()) {
         return Result.Err({
