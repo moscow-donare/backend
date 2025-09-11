de29626ec5aefc7925b55b72f0b812c9f6c3ae20
stefano <stefano@xcapit.com>
2025-09-11 20:34:07 -0300

    refactor: improve error handling in cancelCampaign and add route for canceling campaigns

--- src/core/campaigns/application/cancelCampaign.ts
+++ src/core/campaigns/application/cancelCampaign.ts
@@ -5,20 +5,28 @@ import listByCriteria from "$shared/core/application/listByCriteria";
 import type { Campaign } from "../domain/campaign";
 import type { InputType } from "src/infraestructure/hono/handlers/campaigns/cancelCampaign";
 import type { User } from "$core/users/domain/user";
+import { CampaignStatus } from "../domain/enums";
 
 export const cancelCampaign = async (cancelCampaignDTO: InputType, user: User, repositories: ContainerCampaignRepository) => {
     const criteria: Criteria = new Criteria();
     criteria.addFilter(new Filter("id", cancelCampaignDTO.id));
 
     const campaign = await listByCriteria<Campaign>(repositories.campaignRepository, criteria);
-    const campaignToCancel = campaign.Unwrap()[0] ?? null;
 
-    if (campaign.IsErr || !campaignToCancel) {
+    if (campaign.IsErr) {
         return Result.Err({
             code: "CAMPAIGN_NOT_FOUND",
             details: campaign.Error || "No campaign found with the given criteria",
         });
     }
+    const campaignToCancel = campaign.Unwrap()[0] ?? null;
+    if (!campaignToCancel) {
+        return Result.Err({
+            code: "CAMPAIGN_NOT_FOUND",
+            details: "No campaign found with the given criteria",
+        });
+    }
+
 
     if (campaignToCancel.creator !== user && !user.isAdmin()) {
         return Result.Err({
@@ -31,6 +39,13 @@ export const cancelCampaign = async (cancelCampaignDTO: InputType, user: User, r
         return Result.Err({
             code: "CAMPAIGN_ALREADY_CANCELED",
             details: "The campaign is already canceled",
+        });
+    }
+
+    if (campaignToCancel.getCurrentStatus() !== CampaignStatus.IN_REVIEW && campaignToCancel.getCurrentStatus() !== CampaignStatus.ACTIVE) {
+        return Result.Err({
+            code: "INVALID_CAMPAIGN_STATUS",
+            details: "Only campaigns in Pending Review or Active status can be canceled",
         });
     }
 
--- src/infraestructure/hono/routes.ts
+++ src/infraestructure/hono/routes.ts
@@ -7,6 +7,7 @@ import editCampaignHandler from "./handlers/campaigns/editCampaign";
 import getCampaignByIdPublicHandler from "./handlers/campaigns/getCampaignByIdPublicHandler";
 import approveCampaignHandler from "./handlers/campaigns/approveCampaign";
 import getCampaignsInReviewHandler from "./handlers/campaigns/getCampigsInReview";
+import cancelCampaignHandler from "./handlers/campaigns/cancelCampaign";
 
 const routes: Route[] = [
   {
@@ -59,6 +60,14 @@ const routes: Route[] = [
       id: "getCampaignByIdPublic",
       url: "/campaigns/:id",
       method: "GET",
+    }
+  },
+  {
+    handler: cancelCampaignHandler,
+    schema: {
+      id: "cancelCampaign",
+      url: "/campaigns/cancel",
+      method: "PATCH",
     }
   },
   // approve campaign 
