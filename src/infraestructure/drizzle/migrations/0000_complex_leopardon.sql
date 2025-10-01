5b5095409970d795908479717efdfd54e80b015c
stefano <stefano@xcapit.com>
2025-09-30 21:18:51 -0300

    refactor: consolidate database migrations and update schema
    - Removed obsolete migration files for users, campaigns, and state_changes.
    - Created a new migration file to establish the campaigns, state_changes, and users tables with updated schema.
    - Added contract_address column to campaigns table.
    - Updated snapshot files to reflect the new database structure.
    - Adjusted journal versioning to align with the new migration setup.

--- src/infraestructure/drizzle/migrations/0000_complex_leopardon.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"full_name" varchar(255) NOT NULL,
-	"email" varchar(255) NOT NULL,
-	"address" varchar(255) NOT NULL,
-	"created_at" timestamp with time zone DEFAULT now(),
-	CONSTRAINT "users_email_unique" UNIQUE("email"),
-	CONSTRAINT "users_address_unique" UNIQUE("address")
-);
--- src/infraestructure/drizzle/migrations/0003_mixed_the_hand.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "state_changes" DROP CONSTRAINT "state_changes_campaign_id_campaigns_id_fk";
